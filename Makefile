ce21e533fa9f4dcb57caa4b487767a6f904aff52
jonkjonks <jennyli12320@gmail.com>
2023-03-26 17:50:35 +1100

    updates

--- Makefile
+++ Makefile
@@ -1,15 +1,35 @@
 include .env
+
+LAVALINK_VERSION ?= 3.7.5
+
+BOT_DOCKER=docker run --user root -it --rm \
+	-v `pwd`:/workdir \
+	-w /workdir \
+	--env-file=.env \
+	-p 80 \
+	node:16 
+
 invite:
 	echo https://discord.com/oauth2/authorize?client_id=$(clientId)&permissions=277083450689&scope=bot%20applications.commands
 
 deploy-bot:
 	npm install && npm run deploy 
 
-get-lavalink:
-	curl -sSL https://github.com/freyacodes/Lavalink/releases/download/3.7.5/Lavalink.jar > lavalink/Lavalink.jar
+get-lavalink: 
+	curl -sSL https://github.com/freyacodes/Lavalink/releases/download/$(LAVALINK_VERSION)/Lavalink.jar > Lavalink.jar
+
+build-lavalink:
+	docker build -t lavalink:latest -f lavalink.Dockerfile .
+
+docker-lavalink: build-lavalink 
+	docker run --env-file=.env --rm \
+	-p $(lavalinkPort):$(lavalinkPort) \
+	-v `pwd`/application.yml:/workdir/application.yml \
+	-w /workdir \
+	lavalink:latest
 
 start-lavalink:
-	(cd lavalink && java -jar Lavalink.jar)
+	java -jar Lavalink.jar
 
 bot: 
 	npm install && npm run start
--- config.js
+++ config.js
@@ -1,12 +1,11 @@
 require("dotenv").config()
-
 module.exports = {// TODO: add description to app.json
 	helpCmdPerPage: 10, //- Number of commands per page of help command
 	lyricsMaxResults: 5, //- Number of results for lyrics command (Do not touch this value if you don't know what you are doing)
-	adminId: process.env.adminId || "", //- Replace UserId with the Discord ID of the admin of the bot
-	token: process.env.token || "", //- Bot's Token
-	clientId: process.env.clientId || "", //- ID of the bot
-	clientSecret: process.env.clientSecret || "", //- Client Secret of the bot
+	adminId: process.env.adminId, //- Replace UserId with the Discord ID of the admin of the bot
+	token: process.env.token, //- Bot's Token
+	clientId: process.env.clientId, //- ID of the bot
+	clientSecret: process.env.clientSecret, //- Client Secret of the bot
 	port: 80, //- Port of the API and Dashboard
 	scopes: ["identify", "guilds", "applications.commands"], //- Discord OAuth2 Scopes
 	inviteScopes: ["bot", "applications.commands"], // Invite link scopes
@@ -21,16 +20,16 @@ module.exports = {// TODO: add description to app.json
 	autoPause: true, //- When set to true, music will automatically be paused if everyone leaves the voice channel
 	autoLeave: false, //- When set to true, the bot will automatically leave when no one is in the voice channel (can be combined with 24/7 to always be in voice channel until everyone leaves; if 24/7 is on disconnectTime will add a disconnect delay after everyone leaves.)
 	debug: false, //- Debug mode
-	cookieSecret: process.env.cookieSecret || "", //- Cookie Secret
-	website: process.env.website || "http://localhost:4200", //- without the / at the end
+	cookieSecret: process.env.cookieSecret, //- Cookie Secret
+	website: process.env.website, //- without the / at the end
 	// You need a lavalink server for this bot to work!!!!
 	// Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
 	nodes: [
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
-			host: process.env.lavalinkHost || "", //- The host name or IP of the lavalink server.
-			port: Number(process.env.lavalinkPort) || 2333, // The port that lavalink is listening to. This must be a number!
-			password: process.env.lavalinkPassword || "", //- The password of the lavalink server.
+			host: process.env.lavalinkHost, //- The host name or IP of the lavalink server.
+			port: Number(process.env.lavalinkPort), // The port that lavalink is listening to. This must be a number!
+			password: process.env.lavalinkPassword, //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 5000, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
